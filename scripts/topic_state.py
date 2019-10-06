edac3a5e4af7242e6060c414d485f41ee5c2f8a2
Lucas Walter <wsacul@gmail.com>
2019-10-06 08:28:30 -0700

    Subscribe to a single topic and publish pulses when it is received.  Next add ability to provide a list of topics

---
+++ scripts/topic_state.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# Copyright (c) 2019 Lucas Walter
+# Subscribe to a list of any type of message, then publish a pulse/toggle for every
+# received message.  This allows easy visualization of the flow of messages
+# on the system when view in rqt_plot (or plotjuggler) similar to viewing
+# the messages in rqt_bag.
+# Furthermore, the output is published as a JointState which can be hooked
+# up to an abstract RobotModel and then the reception of messages can be visualized
+# in rviz.
+
+import rospy
+import rostopic
+
+from roslib.message import get_message_class
+from rospy.msg import AnyMsg
+from sensor_msgs.msg import JointState
+
+
+class TopicState(object):
+    def __init__(self):
+        # TODO(lucasw) support many topics in param list
+        # for now just one
+
+        self.js = JointState()
+        self.js.name.append('topic')
+        self.js.position.append(0.0)
+
+        # self.label = rospy.get_param("~label", "topic")
+        self.pulse_period = rospy.get_param("~period", 0.02)
+        self.edge_time = rospy.get_param("~edge_time", 0.002)
+        self.end_pulse_timer = None
+        self.js_pub = rospy.Publisher('topic_state', JointState, queue_size=10)
+        self.sub = rospy.Subscriber('topic', AnyMsg, self.callback, queue_size=4)
+
+    def callback(self, msg):
+        # TODO(lucasw) if there is a header use that timestamp optionally
+        self.start_pulse()
+
+    def start_pulse(self, event=None):
+        if self.end_pulse_timer is not None:
+            self.end_pulse_timer.shutdown()
+        self.pub(0.0)
+        rospy.sleep(rospy.Duration(self.edge_time))
+        self.pub(1.0)
+        self.end_pulse_timer = rospy.Timer(rospy.Duration(self.pulse_period),
+                                           self.end_pulse,
+                                           oneshot=True)
+
+    def end_pulse(self, event=None):
+        self.pub(1.0)
+        rospy.sleep(rospy.Duration(self.edge_time))
+        self.pub(0.0)
+        self.end_pulse_timer = None
+
+    def pub(self, value=0.0, stamp=None):
+        self.js.header.stamp = stamp
+        if stamp is None:
+            self.js.header.stamp = rospy.Time.now()  # or event.cur_real
+        self.js.position[0] = value
+        self.js_pub.publish(self.js)
+        self.one_shot = None
+
+if __name__ == '__main__':
+    rospy.init_node('topic_state')
+    topic_state = TopicState()
+    rospy.spin()
