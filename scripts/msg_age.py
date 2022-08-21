47ede7a326a84b23c0b71edadfb5659c9652e621
Lucas Walter <wsacul@gmail.com>
2022-08-21 09:38:55 -0700

    measure the average message age for topics with headers for any number of topics

---
+++ scripts/msg_age.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# Copyright (c) 2022 Lucas Walter
+#
+# subscribe to multiple topics with headers and average their age with
+
+import sys
+from threading import Lock
+
+import numpy as np
+import rospy
+from roslib.message import get_message_class
+
+
+class MsgAge(object):
+    def __init__(self, args):
+        topics = []
+        for arg in args:
+            if arg[0] == '_':
+                continue
+            topics.append(arg)
+
+        rospy.loginfo(f"getting age of topics: {topics}")
+
+        self.lock = Lock()
+
+        self.sync_sub = None
+        self.topics = {}
+        self.topic_types = {}
+        self.classes = {}
+        self.subs = {}
+        self.ages = {}
+        for index in range(len(topics)):
+            self.topics[index] = topics[index]
+            self.subs[index] = rospy.Subscriber(self.topics[index], rospy.AnyMsg,
+                                                self.init_callback,
+                                                callback_args=(index),
+                                                queue_size=1)
+            self.ages[index] = []
+
+        period = rospy.get_param("~period", 1.0)
+        self.timer = rospy.Timer(rospy.Duration(period), self.update)
+
+    def init_callback(self, msg, args):
+        index = args  # args[0]
+        topic_type = msg._connection_header['type']
+        self.topic_types[index] = topic_type
+        self.subs[index].unregister()
+
+        # TODO(lucasw) reject if message doesn't have a header
+        topic_class = get_message_class(topic_type)
+        rospy.loginfo(f"{index} found class for '{topic_type}': {topic_class}")
+        self.subs[index] = rospy.Subscriber(self.topics[index], topic_class,
+                                            self.callback,
+                                            callback_args=(index),
+                                            queue_size=10)
+
+    def callback(self, msg, args):
+        cur = rospy.Time.now()
+        index = args
+        with self.lock:
+            self.ages[index].append((cur - msg.header.stamp).to_sec())
+
+    def update(self, event):
+        text = "ages:"
+        with self.lock:
+            for index, ages in self.ages.items():
+                if len(ages) == 0:
+                    age_str = "no messages"
+                else:
+                    age_str = f" average age {np.mean(ages):0.4f}s over {len(ages)} messages"
+                text += f"\n{index} {self.topics[index]} {self.topic_types[index]} {age_str}"
+                self.ages[index] = []
+        rospy.loginfo(text)
+
+
+if __name__ == '__main__':
+    rospy.init_node('msg_age')
+    node = MsgAge(sys.argv[1:])
+    rospy.spin()
