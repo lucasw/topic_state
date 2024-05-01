8bf6638bed951c802a6153c273a21957548464f6
Lucas Walter <wsacul@gmail.com>
2024-05-01 12:06:08 -0700

    optionally write a bag of the elapsed durations between a header stamp and rosbag received time

--- scripts/bag_msg_age.py
+++ scripts/bag_msg_age.py
@@ -6,17 +6,26 @@ Look at every message with a header in a bag and computer per-topic statistics
 for the age of the messages
 """
 
+import os
 import sys
 
 import numpy as np
 import rosbag
 import rospy
+from marti_common_msgs.msg import DurationStamped
 
 
 def main():
     rospy.init_node("bag_msg_age")
     argv = [arg for arg in sys.argv[1:] if not arg.startswith("_")]
     bags = [arg for arg in argv if arg.endswith(".bag")]
+
+    # write DurationStamped to bag
+    elapsed_bag = None
+    write_bag = rospy.get_param("~write_bag", True)
+    output_bag_name = rospy.get_param("~output_bag_name", "durations.bag")
+    if write_bag:
+        elapsed_bag = rosbag.Bag(output_bag_name + ".active", "w")
 
     ages_by_topic = {}
 
@@ -36,12 +45,19 @@ def main():
                     continue
 
                 header_stamp = message.header.stamp
-                elapsed = rx_stamp - header_stamp
+                elapsed = DurationStamped()
+                elapsed.header.stamp = header_stamp
+                elapsed.value = rx_stamp - header_stamp
 
                 if topic not in ages_by_topic:
                     rospy.loginfo(topic)
                     ages_by_topic[topic] = []
-                ages_by_topic[topic].append(elapsed.to_sec())
+                ages_by_topic[topic].append(elapsed.value.to_sec())
+                if elapsed_bag is not None:
+                    elapsed_bag.write(topic + "/elapsed", elapsed, elapsed.header.stamp)
+
+    elapsed_bag.close()
+    os.rename(output_bag_name + ".active", output_bag_name)
 
     print()
     rospy.loginfo("topic len min mean max")
