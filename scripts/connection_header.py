f997a82285055c374e7ceb0e1cade0ea78ee6724
Lucas Walter <wsacul@gmail.com>
2024-07-07 06:36:41 -0700

    Use argv so less typing to get connection header

--- scripts/connection_header.py
+++ scripts/connection_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+import sys
 
 import rospy
 from rospy.msg import AnyMsg
 
 
 rospy.init_node("connection_header")
-msg = rospy.wait_for_message("topic", AnyMsg)
+msg = rospy.wait_for_message(sys.argv[1], AnyMsg)
 print(dir(msg))
 print(msg._connection_header)
