e91e9b35cd835be4c32b77ea885be88070928174
Lucas Walter <wsacul@gmail.com>
2024-08-23 09:16:45 -0700

    format ooutput of connection header differently

--- scripts/connection_header.py
+++ scripts/connection_header.py
@@ -6,6 +6,14 @@ from rospy.msg import AnyMsg
 
 
 rospy.init_node("connection_header")
-msg = rospy.wait_for_message(sys.argv[1], AnyMsg)
-print(dir(msg))
-print(msg._connection_header)
+topic = sys.argv[1]
+print(topic)
+msg = rospy.wait_for_message(topic, AnyMsg)
+# print(dir(msg))
+print(f"type: {msg._type}")
+print("connection header:")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+for key, value in msg._connection_header.items():
+    print(f"{key}:\n{value}")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print()
